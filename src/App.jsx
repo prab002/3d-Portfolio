cb61a5754e76a8df08aca7b9b8ba526a5dd0d216
prab <mprabhanjan18@gmail.com>
2024-09-15 11:44:19 +0530

    added the project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <h1 className="text-2xl text-white underline">Hello, Prab portfolio</h1>
+    </main>
+  );
+};
+
+export default App;
